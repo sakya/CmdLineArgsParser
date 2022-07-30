f55a46037439ecd756b0b90e1a6a9a8a4c906f19
Paolo Iommarini <paolo.iommarini@live.com>
2022-07-30 19:12:24 +0200

    Fixed ShowUsage

--- CmdLineArgsParser/Parser.ShowUsage.cs
+++ CmdLineArgsParser/Parser.ShowUsage.cs
@@ -45,83 +45,91 @@ namespace CmdLineArgsParser
             // Options
             // Verb first
             if (verb != null && verb.HasValuesList) {
-                Console.WriteLine(string.IsNullOrEmpty(verb.Option.Name) ? "Verb:" : $"{verb.Option.Name.Capitalize()}:");
-                if (verb.Option.ValidValues?.Length > 0) {
-                    foreach (var v in verb.Option.ValidValues) {
-                        Console.WriteLine($"  {v}");
-                    }
-                } else {
-                    var propertyType = GetOptionBaseType(verb.Property.PropertyType);
-                    if (propertyType != null) {
-                        var enumValues = Enum.GetNames(propertyType);
-                        foreach (var v in enumValues) {
-                            Console.Write($"  {v}{ new string(' ', columnsForName - v.Length - 2) }");
-
-                            var enumVal = Enum.Parse(propertyType, v);
-                            var type = enumVal.GetType();
-                            var memInfo = type.GetMember(enumVal.ToString());
-                            var attribute = memInfo[0].GetCustomAttribute(typeof(DescriptionAttribute), false) as DescriptionAttribute;
-                            if (attribute != null) {
-                                var desc = attribute.Description;
-                                var wrappedText = desc.WordWrap(Console.WindowWidth - columnsForName).TrimEnd();
-                                var lines = wrappedText.Split(new[] { Environment.NewLine }, StringSplitOptions.None);
-                                for (int i = 0; i < lines.Length; i++) {
-                                    var line = lines[i];
-                                    if (i == 0)
-                                        Console.Write(line);
-                                    else {
-                                        Console.WriteLine();
-                                        Console.Write($"{new string(' ', columnsForName)}{line}");
-                                    }
-                                }
-                            }
-                            Console.WriteLine();
-                        }
-                    }
-                }
-
-                Console.WriteLine();
+                ShowVerbUsage(verb, columnsForName);
             }
 
             var sections = properties.GroupBy(p => p.Option.Section);
             foreach (var section in sections) {
                 Console.WriteLine($"{ (string.IsNullOrEmpty(section.Key) ? "General" : section.Key) }:");
-                foreach (var property in section.OrderBy(p => p.Option.Name)) {
-                    var opt = property.Option;
-                    if (opt.Verb)
-                        continue;
-
-                    StringBuilder sb = new StringBuilder();
-                    sb.Append("  ");
-                    if (opt.ShortName != '\0') {
-                        sb.Append($"-{opt.ShortName}, ");
-                    }
-
-                    sb.Append($"--{opt.Name}");
-                    if (property.Property.PropertyType != typeof(bool))
-                        sb.Append("=VALUE");
-                    if (sb.Length < columnsForName) {
-                        sb.Append(new string(' ', columnsForName - sb.Length));
-                    } else {
-                        sb.AppendLine();
-                        sb.Append(new string(' ', columnsForName));
-                    }
-
-                    if (!string.IsNullOrEmpty(opt.Description)) {
-                        var wrappedText = opt.Description.WordWrap(Console.WindowWidth - columnsForName).TrimEnd();
-                        var lines = wrappedText.Split(new[] { Environment.NewLine }, StringSplitOptions.None);
-                        for (int i = 0; i < lines.Length; i++) {
-                            var line = lines[i];
-                            if (i == 0)
-                                sb.Append(line);
-                            else
-                                sb.Append($"{Environment.NewLine}{new string(' ', columnsForName)}{line}");
-                        }
-                    }
-
-                    Console.WriteLine(sb.ToString());
+                foreach (var property in section.Where(p => !p.Option.Verb).OrderBy(p => p.Option.Name)) {
+                    ShowOptionUsage(property, columnsForName);
                 }
             }
+        }
+
+        private void ShowVerbUsage(OptionProperty verb, int columnsForName)
+        {
+            Console.WriteLine(string.IsNullOrEmpty(verb.Option.Name) ? "Verb:" : $"{verb.Option.Name.Capitalize()}:");
+            if (verb.Option.ValidValues?.Length > 0) {
+                foreach (var v in verb.Option.ValidValues) {
+                    Console.WriteLine($"  {v}");
+                }
+            } else {
+                var propertyType = GetOptionBaseType(verb.Property.PropertyType);
+                if (propertyType != null) {
+                    var enumValues = Enum.GetNames(propertyType);
+                    foreach (var v in enumValues) {
+                        Console.Write($"  {v}{ new string(' ', columnsForName - v.Length - 2) }");
+
+                        var enumVal = Enum.Parse(propertyType, v);
+                        var type = enumVal.GetType();
+                        var memInfo = type.GetMember(enumVal.ToString());
+                        var attribute = memInfo[0].GetCustomAttribute(typeof(DescriptionAttribute), false) as DescriptionAttribute;
+                        if (attribute != null) {
+                            var desc = attribute.Description;
+                            var wrappedText = desc.WordWrap(Console.WindowWidth - columnsForName).TrimEnd();
+                            var lines = wrappedText.Split(new[] { Environment.NewLine }, StringSplitOptions.None);
+                            for (int i = 0; i < lines.Length; i++) {
+                                var line = lines[i];
+                                if (i == 0)
+                                    Console.Write(line);
+                                else {
+                                    Console.WriteLine();
+                                    Console.Write($"{new string(' ', columnsForName)}{line}");
+                                }
+                            }
+                        }
+                        Console.WriteLine();
+                    }
+                }
+            }
+
+            Console.WriteLine();
+        }
+
+        private void ShowOptionUsage(OptionProperty property, int columnsForName)
+        {
+            var opt = property.Option;
+
+            StringBuilder sb = new StringBuilder();
+            sb.Append("  ");
+            if (opt.ShortName != '\0') {
+                sb.Append($"-{opt.ShortName}, ");
+            }
+
+            sb.Append($"--{opt.Name}");
+            if (property.Property.PropertyType != typeof(bool))
+                sb.Append("=VALUE");
+            if (sb.Length < columnsForName) {
+                sb.Append(new string(' ', columnsForName - sb.Length));
+            } else {
+                sb.AppendLine();
+                sb.Append(new string(' ', columnsForName));
+            }
+
+            if (!string.IsNullOrEmpty(opt.Description)) {
+                var wrappedText = opt.Description.WordWrap(Console.WindowWidth - columnsForName).TrimEnd();
+                var lines = wrappedText.Split(new[] { Environment.NewLine }, StringSplitOptions.None);
+                for (int i = 0; i < lines.Length; i++) {
+                    var line = lines[i];
+                    if (i == 0)
+                        sb.Append(line);
+                    else
+                        sb.Append($"{Environment.NewLine}{new string(' ', columnsForName)}{line}");
+                }
+            }
+
+            Console.WriteLine(sb.ToString());
         }
     }
 }
